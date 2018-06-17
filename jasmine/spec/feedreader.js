0e0b794ff305237f18e45d12501984e462425a36
Evangelos Kalabokis <kalabokisevangelos@gmail.com>
2018-06-17 23:15:38 +0300

    Wrote Initial Entries test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -34,8 +34,7 @@ $(function() {
         it('url defined and not empty', function () {
           allFeeds.forEach(function(feed) {
             expect(feed.url).toBeDefined();
-            expect(feed.url).not.toBe(null);
-            expect(feed.url).not.toBe("");
+            expect(feed.url.length).toBeGreaterThan(0);
           })
         });
 
@@ -46,8 +45,7 @@ $(function() {
          it('name defined and not empty', function () {
            allFeeds.forEach(function(feed) {
              expect(feed.name).toBeDefined();
-             expect(feed.name).not.toBe(null);
-             expect(feed.name).not.toBe("");
+             expect(feed.name.length).toBeGreaterThan(0);
            })
          });
 
@@ -98,6 +96,7 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -105,6 +104,22 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         beforeEach(function(done) {
+           loadFeed(0, function() {
+             done();
+           });
+         });
+
+         it('feed container not empty', function(done) {
+           let feedContainer = document.querySelector(".feed");
+           let entries = feedContainer.querySelectorAll(".entry");
+
+           expect(entries.length).not.toBe(0);
+           done();
+
+         });
+
+       });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
@@ -112,4 +127,5 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+
 }());
